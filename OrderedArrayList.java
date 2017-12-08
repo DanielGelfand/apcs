d541b2491d7706123c99113e1ce5a54989a7f90f
DanielGelfand <33498710+DanielGelfand@users.noreply.github.com>
2017-12-07 21:00:06 -0500

    OrderedArrayList with Binary Search

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,7 +1,7 @@
 //Daniel Gelfand
 //APCS1 pd2
-//HW47 -- ascending,making wrapper class
-//2017-12-05
+//HW49 -- Halving the Halves
+//2017-12-07
 
 
 /********************************
@@ -16,107 +16,116 @@ import java.util.ArrayList;
 
 public class OrderedArrayList
 {
-    // instance of class ArrayList, holding objects of type Comparable 
-    // (ie, instances of a class that implements interface Comparable)
-    private ArrayList<Comparable> _data;
+  // instance of class ArrayList, holding objects of type Comparable 
+  // (ie, instances of a class that implements interface Comparable)
+  private ArrayList<Comparable> _data;
 
 
-    // default constructor initializes instance variable _data
-    public OrderedArrayList()
-    {
-	_data = new ArrayList<Comparable>();
-    }
+  // default constructor initializes instance variable _data
+  public OrderedArrayList()
+  {
+    _data = new ArrayList<Comparable>();    
+  }
 
 
-    public String toString()
-    {
-	String foo = "[";
-	for( int i = 0; i < size(); i++ ) {
-	    foo += get(i) + ",";
-	}
-	if ( foo.length() > 1 )
-	    //shave off trailing comma
-	    foo = foo.substring( 0, foo.length()-1 );
-	foo += "]";
+  public String toString()
+  {
+    return _data.toString(); 
+  }
+
+
+  public Comparable remove( int index )
+  {	
+    return _data.remove(index); 
+  }
+
+
+  public int size()
+  { 
+    return _data.size();
+  }
+
     
-	return foo;
-   
-    }
+  public Comparable get( int index )
+  { 
+    return _data.get(index); 
+  }
 
 
-    //uses remove() from ArrayList
-    public Comparable remove( int index )
-    {	
-	Comparable temp = get(index);
-
-
-	_data.remove(index);
-	
-	return temp; //placeholder 
-        
-    }
-
-      
-   
-
-
-    //uses size() from ArrayList
-    public int size()
-    { 
-	return _data.size();
-    }
-
-    //uses get() from ArrayList
-    public Comparable get( int index )
-    { 
-   
-	return _data.get(index); 
-    }
-
-
-    //uses add methods from ArrayList
-    public void add(Comparable newVal)
-    {
-	for(int i = 0; i < size(); i +=1){
-	    //if newVal is smaller than elem at a certain index add it at that index
-	    if(get(i).compareTo(newVal) >= 0){
-		_data.add(i,newVal);
-	       
-	      	return; //add newVal at proper position and get out of method
-	        
+  /***
+   * add takes as input any comparable object 
+   * (i.e., any object of a class implementing interface Comparable)
+   * inserts newVal at the appropriate index
+   * maintains ascending order of elements
+   * uses a linear search to find appropriate index
+   ***/
+  public void addLinear( Comparable newVal )
+  { 
+    for( int p = 0; p < _data.size(); p++ ) {
+	    if ( newVal.compareTo( _data.get(p) ) < 0 ) { 
+        //newVal < oal[p]
+        _data.add( p, newVal );
+        return; //Q:why not break?
 	    }
-	  
-	}
-    
-       	_data.add(newVal); //otherwise append to the end
-    
+    }
+    _data.add( newVal ); //newVal > every item in oal, so add to end 
     }
 
 
+    public void addBin(Comparable newVal){
 
-    // main method solely for testing purposes
-    public static void main( String[] args )
-    {
-  
-	OrderedArrayList Franz = new OrderedArrayList();
+	int lo = 0;
+	int hi = size()-1;
+	int mid;
 
-	// testing linear search
-	for( int i = 0; i < 15; i++ )
-	    Franz.add( (int)( 50 * Math.random() ) );
-	System.out.println( Franz );
-	// System.out.println(Franz.size()); //
-  
-	//check for sorted-ness
-	//if msg does not appear, list was sorted
-	for( int i=0; i<Franz.size()-1; i++ ) {
-	    System.out.println("at i: " + Franz.get(i) );
-	    if ( Franz.get(i).compareTo(Franz.get(i+1)) > 0 ) {
-		System.out.println( " *** NOT sorted *** " );
-		break;
+	while(lo <= hi){
+
+	    //get middle
+	    mid = (lo+hi) / 2;
+
+	    //variable holds whether newVal is equal, less than, or greater than elem at mid
+	    int check = newVal.compareTo(get(mid));
+	    
+	    if(check == 0){
+		_data.add(mid,newVal);
+	    }
+
+	    //if newVal greater than elem at mid look at the right
+	    else if(check > 0){
+		lo = mid + 1;
+
+	    }
+	    //if newVal less than elem at mid look at the left
+	    else{
+
+		hi = mid - 1;
 	    }
 	}
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-	   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-    }//end main()
+	//if no appropriate spot found add to end
+	_data.add(lo,newVal);
+    }
+
+  // main method solely for testing purposes
+  public static void main( String[] args )
+  {
+    OrderedArrayList Franz = new OrderedArrayList();
+
+    // testing linear search
+    for( int i = 0; i < 3; i++ )
+      Franz.addBin( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+
+    //check for sorted-ness
+    //if msg does not appear, list was sorted
+    for( int i=0; i<Franz.size()-1; i++ ) {
+      System.out.println("at i: " + Franz.get(i) );
+      if ( Franz.get(i).compareTo(Franz.get(i+1)) > 0 ) {
+        System.out.println( " *** NOT sorted *** " );
+        break;
+      }
+    }
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  }//end main()
 
 }//end class OrderedArrayList
